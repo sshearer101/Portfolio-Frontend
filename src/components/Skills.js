20ea2184aed1a4ff9c2fae77cf597443b9b774fb
Sam Shearer <sshearer101@gmail.com>
2022-03-30 19:04:27 -0600

    css

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -2,27 +2,41 @@ import React from 'react'
 import { skills } from '../data'
 
 export default function Skills() {
+
+
   return (
     <div>
       <div className="skills-intro">
         <h1>Skills &amp; Technologies </h1>
         <p>
-          Some of the technologies that I have used in projects, assignments, and other coding practices include the following: 
+          Since beginning the pursuit of my tech career in July of 2021, I have worked with both frontend and backend languages. 
+          <br></br>
+          <br></br>
+          I have been able to create responsive user-friendly websites that cater to my interests and are built with best practices. My main area of expertise is with frontend development languages like HTML, CSS, and JavaScript to build small web apps that provide animations, interactive platforms, and interact with backend servers.
+          <br></br>
+          <br></br>
+          I also have experience with backend languages like Ruby on Rails. 
+          <br></br>
+          <br></br>
+          {/* Visit my LinkedIn profile for more details to contact me! */}
+          Visit my <a href="https://www.linkedin.com/in/sam-shearer-101/"> LinkedIn</a> for more details to contact me
+        
+
         </p>
        
 
       </div>
       <div className="skills-info">
         {skills.map((skill) => (
-          <img alt="skill-symbol" src={skill.skill}/>
+          <img alt="skill-symbol" src={skill.skill} className="skill-symbol"/>
         ))}
       </div>
-
+{/* 
     <div className="skills-intro-two">  
     <p >
       Other skills and experience include:
     </p>
-    <ul>
+    <ul className='skill-list'>
       <li> Microsoft Word</li>
       <li> Microsoft PowerPoint</li>
       <li> Microsoft Excel</li>
@@ -30,7 +44,7 @@ export default function Skills() {
       <li> Github</li>
 
     </ul>
-    </div>   
+    </div>    */}
     </div>
   )
 }
