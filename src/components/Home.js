9a7e986f099cbb420734aa69e245c5f179a37ad3
Sam Shearer <sshearer101@gmail.com>
2022-04-29 16:48:18 -0600

    navbar

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,7 +10,7 @@ export default function Home() {
             Full Stack Software Developer
           </div>
         </div>
-      </div>{' '}
+      </div>
     </div>
   )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,16 +4,16 @@ export default function Navbar() {
   return (
 
 
-        <ul class="menu">
+    <ul class="menu">
     <li class="menu_list">
         <span className="front fas fa-home"></span>
-        <Link to="/" className="side">Home</Link>    </li>
+        <Link to="/" className="side">Home</Link></li>
     <li class="menu_list">
         <span className="front fas fa-info"></span>
         <Link to="/about" className='side'>About</Link>
     </li>
     <li class="menu_list">
-        <span className="front fas fa-briefcase"></span>
+        <span className="front fas fa-book-open"></span>
         <Link to="/skills" className="side"> Skills </Link>
     </li>
     <li class="menu_list">
